aac7ea59a71a814424d62f18569b695d24fdb31a
Dean Pugh <dean.pugh@15gifts.com>
2018-10-21 20:59:48 +0100

    Add in readme. Get speach detection and utterance working in basic mode with Voice list

---
+++ readme.md
@@ -0,0 +1,27 @@
+# Iris
+
+Welcome to the in progress build of Iris. An AI which can complete tasks via speech and text commands. 
+
+Hopefully, you'll be able to have a full conversation with Iris, which can be personalised to work with a variety of external sources.
+
+## Current ability
+
+At this moment, this is just proof of concept. You can start the script in a chrome web browser, or anything else which supports the Speech Systhesis API. 
+
+The only command it recognises is 'Iris', which will give you a different response, but end the converstation.
+
+## Getting started
+
+Clone the repo somewhere and then run `npm install`.
+
+To run the demo, enter the following command which should open a web browser and the demo page.
+
+```
+npm run dev
+```
+
+## Future development
+
+There are a couple of technologies that I have planned to incorporate into this project. As with each, there are various different types in which we can use. As I review them and finally settle on each, they will appear in the list below.
+
+* (Artoym JS)[https://github.com/sdkcarlos/artyom.js] HTML5 Voice Control
--- src/components/App.js
+++ src/components/App.js
@@ -2,47 +2,149 @@ import React from 'react'
 
 class App extends React.Component {
     state = {
-        command: ''
+        command: '',
+        speak: true,
+        voices: [],
+        speechUtterance: null,
     }
 
-    componentDidMount () {
+    initSpeech () {
+        this.initSpeechRecognition()
+        this.initSpeechUtterance()
+    }
+    
+    initSpeechRecognition () {
         window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
-
+    
         const recognition = new window.SpeechRecognition()
         recognition.interimResults = true
-
         recognition.lang = 'en-US'
-
+        
         let p = document.createElement('p')
         const words = document.querySelector('.words')
         words.appendChild(p)
+    
+        recognition.addEventListener('result', (e) => { 
+            this.speechProcess(e) 
+        })
+        recognition.addEventListener('end', recognition.start)
+        recognition.start()
+    }
 
-        recognition.addEventListener('result', e => {
-            const transcript = Array.from(e.results)
-                .map(result => result[0])
-                .map(result => result.transcript)
-                .join('')
+    initSpeechUtterance () {
+        const { speechUtterance } = this.state
+        if (!speechUtterance) {
+            var newSpeechUtterance = new SpeechSynthesisUtterance()
+            this.setState({
+                speechUtterance: newSpeechUtterance
+            })
+        }
+    }
 
+    welcomeMessage() {
+        this.response('Hello, and welcome to Iris Version 1. Who am I talking to?')
+    }
+
+    speechProcess = (e) => {
+        console.log('Speech process')
+
+        const transcript = Array.from(e.results)
+            .map(result => result[0])
+            .map(result => result.transcript)
+            .join('')
+    
+        if (e.results[0].isFinal) {
+            console.log('Final')
+            console.log(transcript)
+            
             this.setState({
                 command: transcript
             })
 
-            if (e.results[0].isFinal) {
-                p = document.createElement('p')
-                words.appendChild(p)
-            }
-        })
-
-        recognition.addEventListener('end', recognition.start)
-
-        recognition.start()
+            this.processCommand()
+        }
     }
+
+    processCommand = () => {
+        console.log('process command')
+        const { command } = this.state
+        
+        var response = ''
+        if (command === 'Iris') {
+            response = 'Hello, how can I help?'
+        } else {
+            response = 'I am sorry, but could you say that again?'
+        }
+
+        console.log(response)
+        this.response(response)
+    }
+
+    response = (response) => {
+        const { speak, speechUtterance, voices } = this.state
+        
+        if (speak) {
+            window.speechSynthesis.cancel()
+            const voiceId = this.refs['voice-selection'].value
+            const voice = voices.find(voice => voice.name === voiceId);
+            speechUtterance.text = response
+            speechUtterance.voice = voice
+
+            window.speechSynthesis.speak(speechUtterance)
+
+        }
+    }
+
+    generateVoices = (e) => {
+        let voices = window.speechSynthesis.getVoices() || []
+
+        voices = voices.filter(voice => voice.lang.includes('en'))
+    
+        this.setState({
+            voices 
+        })
+    }
+
+    componentDidMount () {
+        this.initSpeech()
+    }
+
+    componentWillMount () {
+        // you have to wait until voices have been loaded
+        // this.generateVoices.bind(this)
+        window.speechSynthesis.onvoiceschanged = () => {
+            console.log('Loaded: speechSynthesis')
+            this.generateVoices(this)
+            this.welcomeMessage()
+        };
+    }
+
     render () {
+        const { command, voices } = this.state
+
+        // convert voices from object to array
+        var voicesArray = Object.keys(voices).map(function (i) {
+            return voices[i];
+        });
+
         return (
             <div className="voice-command">
                 <h1>Welcome to Ir15</h1>
-                <p>Talk, what you have said will appear below:</p>
-                <div className="words">{this.state.command}</div>
+                <p>Say 'Iris' to start conversation:</p>
+                <div className="words">{ command }</div>
+                <select className="voice-selection" ref="voice-selection">
+                { voicesArray.map((voice, i) => {
+                    return (
+                    <option value={voice.voiceURI}
+                        key={i}>
+                        { `${voice.name} (${voice.lang})` }
+                    </option>
+                    )
+                })}
+                </select>
+                <button onClick={this.processCommand}>
+                    Test
+                </button>
             </div>
         )
     }
